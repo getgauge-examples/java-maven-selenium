dc8dd49c78f29192876a1840d0bc7cef222bb5b5
Osanda Deshan <osanda.deshan@gmail.com>
2020-03-29 21:35:49 +0530

    Update selenium, bonigarcia dependencies to latest Extend DriverFactory to support Edge browser Format specs
    Signed-off-by: Osanda Deshan <osanda.deshan@gmail.com>

--- specs/PlaceOrder.spec
+++ specs/PlaceOrder.spec
@@ -1,18 +1,17 @@
-Place an Order
-==============
+# Place an Order
 
 * Go to active admin store
 
-Buy a book
-----------
+## Buy a book
+
 tags: customer
 
 * Log in with customer name "ScroogeMcduck" and "password"
 * Place order for "Beginning Ruby: From Novice to Professional". The cart should now contain "1" items
 * Log out
 
-Cart retains items until order is placed
-----------------------------------------
+## Cart retains items until order is placed
+
 tags: customer
 
 * Log in with customer name "ScroogeMcduck" and "password"
--- src/test/java/utils/driver/DriverFactory.java
+++ src/test/java/utils/driver/DriverFactory.java
@@ -3,15 +3,16 @@ package utils.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class DriverFactory {
+class DriverFactory {
 
     // Get a new WebDriver Instance.
     // There are various implementations for this depending on browser. The required browser can be set as an environment variable.
     // Refer http://getgauge.io/documentation/user/current/managing_environments/README.html
-    public static WebDriver getDriver() {
+    static WebDriver getDriver() {
 
         String browser = System.getenv("BROWSER");
         if (browser == null) {
@@ -23,6 +24,9 @@ public class DriverFactory {
             case "IE":
                 WebDriverManager.iedriver().setup();
                 return new InternetExplorerDriver();
+            case "EDGE":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();
             case "FIREFOX":
                 WebDriverManager.firefoxdriver().setup();
                 return new FirefoxDriver();
